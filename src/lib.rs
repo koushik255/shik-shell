7415fc8a02ee07ef0ca6a8c4eb8f1d476c5a6c74
koushik <191687017+koushik255@users.noreply.github.com>
2025-12-29 20:05:50 -0500

    dwada

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-use itertools::Itertools;
 use rayon::prelude::*;
+
 use std::fs::read_dir;
 
-use std::time::Instant;
 use std::{collections::HashMap, path::PathBuf};
 
 pub fn dir_list_one(path: &str, extention: String, dir: bool) -> Vec<FilePlus> {
@@ -33,63 +32,24 @@ pub fn dir_list_one(path: &str, extention: String, dir: bool) -> Vec<FilePlus> {
         }
     });
 
-    //let files_clone = files.i.map(|(e, _)| e.to_owned().to_owned());
-    // YEAH THIS BLOCK UNDER ME IS TRASH
-    //
-    //
-    // geting the files into owned
     let mut files_clone: Vec<PathBuf> = files
         .iter()
         .filter_map(|(f, _)| Some(f.to_owned().to_owned()))
         .by_ref()
         .collect();
-    // files_clone = Vec<PathBuf>
 
-    //
-    //let mut files_clone22: Vec<PathBuf> = files.to_owned().to_owned().keys();
-    //
     files_clone.sort();
-
-    // let mut files_extenstion_unique: HashMap<PathBuf, i32> = HashMap::new();
-
-    // let mut fuckyou: Vec<_> = files
-    //     .iter()
-    //     .filter_map(|(f, _)| {
-    //         // getting the extension
-    //         let didwa = f
-    //             .extension()
-    //             .map(|e| e.to_os_string().into_string().unwrap());
-    //
-    //         //need this to insert
-    //         let wtbr = match didwa.clone() {
-    //             Some(t) => t,
-    //             None => "nada".to_string(),
-    //         };
-    //         // this whole blok is just putting the extentions in a hashmap to check if their unique
-    //         files_extenstion_unique.insert(wtbr.clone(), 0);
-    //         didwa
-    //     })
-    //     .collect();
-    //
-
-    //let mut value = files_extenstion_unique.clone();
 
     let unique: HashMap<String, i32> = files
         .into_keys()
         .filter_map(|p| {
             let ext = p.extension()?.to_string_lossy().into_owned();
+
+            // why not just check in here?
+
             Some((ext, 0)) // key-value pair for collect()
         })
         .collect();
-    // i can put checkdupes/vreating fps into here because im already itering over it
-    //
-    // ORDER OF ITER
-    // so itering to collect key values
-    // then im itering again to create a fileplus vec
-    // then im iterting over the fileplus vec
-    //
-    // what if instead if just iterd over the extenstion and just checked if the extenstionending
-    // with &this_exention and if it is then add that to vec plus
 
     let mut fp: Vec<FilePlus> = Vec::new();
     {
@@ -97,33 +57,18 @@ pub fn dir_list_one(path: &str, extention: String, dir: bool) -> Vec<FilePlus> {
     }
 
     let mut all_this: Vec<FilePlus> = Vec::new();
-    //
-    // for full in fp {
-    //     for s in unique.keys() {
-    //         if *s == full.extenstion {
-    //             if full.extenstion.ends_with(&this_exention) {
-    //                 all_this.push(full.clone());
-    //                 // so this does work
-    //             }
-    //         }
-    //     }
-    // }
-    //
+
+    //// try par iter here
     fp.iter().for_each(|full| {
+        //println!("{}", full.full_path.display());
+
         if full.extenstion.ends_with(&this_exention) {
             all_this.push(full.to_owned());
         }
+        //println!("yep");
     });
-
-    // if dir == false {
-    // for this in all_this.clone() {
-    //     println!("o file {}", this.full_path.display(),);
-    // }
-    // } else {
-    //     for this in all_this.clone() {
-    //         println!("m file {}", this.full_path.display());
-    //     }
-    // }
+    //
+    // let gamba: Rc<Cell<Vec<FilePlus>>> = Rc::new(Cell::new(Vec::<FilePlus>::new()));
 
     if !dir {
         let more = walk_dir(dirs.clone(), this_exention.as_str());
@@ -131,15 +76,7 @@ pub fn dir_list_one(path: &str, extention: String, dir: bool) -> Vec<FilePlus> {
         let mut ddidy = more;
         ddidy.sort();
         ddidy.iter().for_each(|f| all_this.push(f.to_owned()));
-        // for m in ddidy {
-        //     //println!("more");
-        //     all_this.push(m);
-        // }
     };
-
-    // for m in more {
-    //     println!("m file {},folder {}", m.full_path.display(), m.extenstion);
-    // }
 
     all_this
 }
@@ -173,6 +110,7 @@ where
 }
 
 // i need to make this a crate somewhere else and just use it in my front end / gui
+// i dont want to work on this anymore to be honest
 
 pub fn walk_dir(dirs: HashMap<&PathBuf, i32>, ext: &str) -> Vec<FilePlus> {
     // what if i made it async parrallelism??
@@ -192,3 +130,7 @@ pub struct FilePlus {
     pub full_path: PathBuf,
     pub extenstion: String,
 }
+
+// where from here, i think its fast enough to where idgaf right now i could probably make it O^N
+// while creating the FilePLus but
+// ok maybe i can implement my own ffempeg or gstremer widget now? ive always wanted to do it
